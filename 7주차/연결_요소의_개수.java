c28f134b1120afbee1e01ae1b3001b9b9d702652
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-03-03 15:45:41 +0900

    Create 연결_요소의_개수.java

---
+++ 7주차/연결_요소의_개수.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 145,480kb / 668ms
+
+public class Main {
+
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // edge
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+        br.close();
+
+        int count = 0;
+        visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int index) {
+        visited[index] = true;
+
+        for (int neighbor : graph.get(index)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor);
+            }
+        }
+    }
+
+}
