05eb8ab900deb16f7dc1173471c5aa2edcc0d567
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-03-10 21:54:32 +0900

    Create 01타일.java

---
+++ 8주차/01타일.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        int[] dp = new int[Math.max(N, 2) + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
