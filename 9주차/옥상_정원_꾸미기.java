13c025e99b03e0a9e2bf15b2174074f30f2f9e72
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-03-11 23:08:13 +0900

    Create 옥상_정원_꾸미기.java

---
+++ 9주차/옥상_정원_꾸미기.java
@@ -0,0 +1,29 @@
+// 22,348kb / 224ms
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		long answer = 0;
+		Deque<Integer> stack = new ArrayDeque<>();
+		for(int i = 0 ; i < N ; i++) {
+			int current = Integer.parseInt(br.readLine());
+			
+			while(!stack.isEmpty() && stack.peek() <= current) {
+				stack.pop();
+			}
+			
+			answer += stack.size();
+			stack.push(current);
+		}
+		
+		System.out.println(answer);
+	}
+}
